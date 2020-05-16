9fbd43a4168ddee269de9343cdbf74408063116f
master <namvpn23121997@gmail.com>
2020-05-16 09:21:00 +0700

    Update

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const HOST = "http://nvshop-server.herokuapp.com"
+export const HOST = "https://nvshop-server.herokuapp.com"
 export const HOST_IMAGE = HOST + "/images/products/"
 
 const instance = axios.create({
